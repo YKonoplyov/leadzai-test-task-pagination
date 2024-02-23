822a7b195da5fa3ebb73e8f1432984ded902396f
Yaroslav Konoplyov <kannabis252@gmail.com>
2024-02-23 13:47:33 +0200

    added big num test

--- tests.py
+++ tests.py
@@ -50,10 +50,16 @@ class TestPaginationGeneration(unittest.TestCase):
             [(12, 26, 3, 0), "1 2 3 ... 12 ... 24 25 26"],
             [(8, 10, 2, 0), "1 2 ... 8 9 10"],
         ]
-        cls.around_overlaping_pagination_ends = [
+        cls.around_overlaping_pagination_ends_case_list = [
             [(5, 8, 0, 3), "... 2 3 4 5 6 7 8"],
             [(12, 26, 0, 12), "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 ..."],
             [(8, 10, 0, 16), "1 2 3 4 5 6 7 8 9 10"],
+        ]
+
+        cls.big_nums_case_list = [
+            [(2000, 10000, 10, 5), "1 2 3 4 5 6 7 8 9 10 ... 1995 1996 1997 1998 1999 2000 2001 2002 2003 2004 2005 ... 9991 9992 9993 9994 9995 9996 9997 9998 9999 10000"],
+            [(142567, 1000000, 8, 6), "1 2 3 4 5 6 7 8 ... 142561 142562 142563 142564 142565 142566 142567 142568 142569 142570 142571 142572 142573 ... 999993 999994 999995 999996 999997 999998 999999 1000000"],
+            [(2465888, 1351266893, 5, 2), "1 2 3 4 5 ... 2465886 2465887 2465888 2465889 2465890 ... 1351266889 1351266890 1351266891 1351266892 1351266893"]
         ]
     
     def test_all_pages_pagination(self):
@@ -103,10 +109,19 @@ class TestPaginationGeneration(unittest.TestCase):
             self.assertEqual(pagination_generator(*test_case_args), expectation)
             for test_case_args, expectation in self.no_around_case_list
         ]        
+    
     def test_around_overlaps_pagination_ends_no_boundaries(self):
         [
             self.assertEqual(pagination_generator(*test_case_args), expectation)
-            for test_case_args, expectation in self.around_overlaping_pagination_ends
-        ]       
+            for test_case_args, expectation in self.around_overlaping_pagination_ends_case_list
+        ]
+
+    def test_big_nums(self):
+        [
+            self.assertEqual(pagination_generator(*test_case_args), expectation)
+            for test_case_args, expectation in self.big_nums_case_list
+        ]
+
+
 if __name__ == "__main__":
     unittest.main()
