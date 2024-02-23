0604ad48180a321df4010880654cb3b7ac626c53
Yaroslav Konoplyov <kannabis252@gmail.com>
2024-02-23 14:18:26 +0200

    added validation tests

--- tests.py
+++ tests.py
@@ -61,7 +61,15 @@ class TestPaginationGeneration(unittest.TestCase):
             [(142567, 1000000, 8, 6), "1 2 3 4 5 6 7 8 ... 142561 142562 142563 142564 142565 142566 142567 142568 142569 142570 142571 142572 142573 ... 999993 999994 999995 999996 999997 999998 999999 1000000"],
             [(2465888, 1351266893, 5, 2), "1 2 3 4 5 ... 2465886 2465887 2465888 2465889 2465890 ... 1351266889 1351266890 1351266891 1351266892 1351266893"]
         ]
-    
+
+        cls.test_validation_case_list = [
+            [("str object", 10, 3, 4), ValueError, "All arguments should be integers"],
+            [(1, 0, 3, 4), ValueError, "`total_pages` shoud be > 0"],
+            [(0, 10, 3, 4), ValueError, "`current_page` shoud be > 0"],
+            [(11, 10, 3, 4), ValueError, "`current_page` should be in range (1, `total_pages`)"],
+            [(5, 10, 2, -1), ValueError, "Around should be >= 0"],
+            [(5, 10, -1, 5), ValueError, "Boundaries arguments should be >= 0"]
+        ]
     def test_all_pages_pagination(self):
         [
             self.assertEqual(pagination_generator(*test_case_args), expectation)
@@ -122,6 +130,9 @@ class TestPaginationGeneration(unittest.TestCase):
             for test_case_args, expectation in self.big_nums_case_list
         ]
 
-
+    def test_validation(self):
+        for test_case_args, error_type, messasge in self.test_validation_case_list:
+            with self.assertRaises(error_type, msg=messasge):
+                pagination_generator(*test_case_args)
 if __name__ == "__main__":
     unittest.main()
