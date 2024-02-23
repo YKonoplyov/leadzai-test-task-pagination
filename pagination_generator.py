e55e53087ad77a75451f1d1dcedc06e449f62e12
Yaroslav Konoplyov <kannabis252@gmail.com>
2024-02-23 14:18:45 +0200

    updated validation

--- pagination_generator.py
+++ pagination_generator.py
@@ -13,13 +13,22 @@ def pagination_list_to_str(pagination_list: list) -> str:
 
 def validate_pagination_args(*pagination_args) -> None:
     current_page, total_pages, boundaries, around = pagination_args
-    
+    if not all([isinstance(pagination_argument, int) for pagination_argument in pagination_args]):
+        raise ValueError("All arguments should be integers")
     if total_pages < 1:
-        raise ValueError("total pages shoud be > 0")
-    if current_page < 1 or current_page > total_pages:
+        raise ValueError("`total_pages` shoud be > 0")
+    if current_page < 1:
         raise ValueError(
-            f"current page ({current_page}) shoud be in range (1, 'total_pages')"
+            f"`current_page`({current_page}) shoud be > 0"
         )
+    if current_page > total_pages:
+        raise ValueError(
+            "`current_page` should be in range (1, `total_pages`)"
+        )
+    if around < 0:
+        raise ValueError("Around should be >= 0")
+    if boundaries < 0:
+        raise ValueError("Boundaries arguments should be >= 0")
 
 
 def generate_left_boundary_and_around(
@@ -92,39 +101,3 @@ def pagination_generator(
     pagination_str = pagination_list_to_str(pagination_list)
     print(pagination_str)
     return pagination_str
-
-
-if __name__ == "__main__":
-    pagination_generator(14, 15, 6, 2)  # 1 ... 4 5
-    pagination_generator(4, 10, 2, 2) # 1 2 3 4 5 6 ... 9 10
-    pagination_generator(3, 5, 2, 2) # 1 2 3 4 5
-    pagination_generator(10, 15, 2, 4)
-    pagination_generator(3, 5, 0, 2)
-    pagination_generator(1, 5, 0, 2)
-    pagination_generator(3, 10, 2, 2) # 1 2 3 4 5 . . . 9 10
-    pagination_generator(5, 20, 2, 2) # 1 2 3 4 5 6 7 . . . 19 20
-    pagination_generator(10, 20, 2, 2) # 1 2 . . . 8 9 10 11 12 . . . 19 20
-    pagination_generator(1, 20, 2, 2) # 1 2 3 . . . 19 20
-    pagination_generator(50, 100, 20, 10)
-    pagination_generator(4, 5, 0, 0)
-    pagination_generator(4, 5, 0, 1)
-    pagination_generator(11, 20, 0, 1)
-    pagination_generator(11, 15, 0, 1)
-    pagination_generator(19, 20, 0, 1)
-    pagination_generator(18, 20, 2, 5)
-    pagination_generator(1, 20, 9, 0)
-    pagination_generator(20, 20, 9, 0)
-    pagination_generator(1, 19, 9, 0)
-    pagination_generator(10, 20, 5, 6)
-    try:
-        pagination_generator(0, 5, 0, 0)
-    except ValueError as e:
-        print("Value error:", e)
-    try:
-        pagination_generator(10, 9, 0, 0)
-    except ValueError as e:
-        print("Value error:", e)
-    try:
-        pagination_generator(1, 0, 0, 0)
-    except ValueError as e:
-        print("Value error:", e)
